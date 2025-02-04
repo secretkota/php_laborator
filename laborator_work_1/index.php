5a1f5e103b67742ee36ead70b4fc8cea208230e5
DeadMan <stanislav.covaliuc@gmail.com>
2025-02-04 17:10:45 +0200

    initial

---
+++ laborator_work_1/index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "Hello world echo<br>";
+print "Hello world print<br>";
+
+$date = 288;
+$message = "Все возвращаются на работу!";
+
+echo  $date.$message."<br>";
+echo "Это сделано при помощи кавычек: $date $message";
