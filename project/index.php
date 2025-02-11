5307e434d5e75a04dc2ddf554990d11f4f20a677
DeadMan <stanislav.covaliuc@gmail.com>
2025-02-11 10:54:39 +0200

    123

---
+++ project/index.php
@@ -0,0 +1,10 @@
+<?php require_once './components/header.php'; ?>
+
+<body class="bg-red-100">
+    <div id="app">
+        My blog appp
+    </div>
+    
+</body>
+
+<?php require_once './components/footer.php'; ?>
